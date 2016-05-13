45914684232c98a5101958a2fabc1110cf411773
LeandroOliveira <leandro.rwd@gmail.com>
2016-05-13 19:48:02 -0300

    blah

--- FechaACaixa/src/T1_FecheACaixaRMI/FecheACaixaInterface.java
+++ FechaACaixa/src/T1_FecheACaixaRMI/FecheACaixaInterface.java
@@ -4,10 +4,10 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface FecheACaixaInterface extends Remote{
-	public  int registraJogador(String nome) throws RemoteException;
+	public int registraJogador(String nome) throws RemoteException;
 	public int encerraPartida (int id) throws RemoteException;
 	public int obtemTabuleiro (int id) throws RemoteException;
 	public int obtemPontuacao (int id) throws RemoteException;
-	public int jogaDados (int id) throws RemoteException;
-	public int selecionaCasas (int id) throws RemoteException;
+	public int[] jogaDados (int id) throws RemoteException;
+	public int selecionaCasas (int id, boolean[] casas) throws RemoteException;
 }
